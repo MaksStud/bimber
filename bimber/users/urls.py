fc553f84f902465493bf6e2045e50b1bc6d73661
Yuriy Hrechka <yuriy.grechka.vitaliyovich@gmail.com>
2025-04-24 15:39:26 +0300

    CHANGED: name of the view and added line in end of file the file

--- bimber/users/urls.py
+++ bimber/users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('auth/', views.ApiAuthorizationView.as_view(), name='user-auth'),
+    path('auth/', views.AuthorizationView.as_view(), name='user-auth'),
 ]
